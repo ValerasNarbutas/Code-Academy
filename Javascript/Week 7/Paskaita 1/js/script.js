76e0fb6cac03b01d67aeb524ecb4b359a284432e
“Augustina <baugustina@gmail.com>
2024-02-26 20:08:41 +0200

    update

--- Javascript/Week 7/Paskaita 1/js/script.js
+++ Javascript/Week 7/Paskaita 1/js/script.js
@@ -1,2 +1,51 @@
-let d = new Date()​
-document.body.innerHTML = "<h1>Today's date is " + d + "</h1>"
+// let d = new Date ();
+// document.body.innerHTML = "<h1>Today's date is " + d + "</h1>"
+
+// let message = -50;
+// let name = "August";
+
+// let someString = 'a sentence';
+
+// // Numbers
+// const birthYear = 1995;
+// const number1 = -55;
+// const number2 = 2.63434;
+
+// // Strings
+// const string1 = "Hello";
+// const string2 = 'kazkas';
+// const string3 = "23453254";
+// const string4 = `Hellooo`;
+
+// // boolean
+// const bool1 = true;
+// const bool2 = false;
+
+// // null
+// const null1 = null;
+
+// // undefined
+// const undefined1 = undefined;
+
+// message = birthYear;
+// alert(message)
+
+// message = 100;
+
+// alert(message)
+
+// UZDUOTYS
+// Darbas su kintamaisiais
+
+// let admin;
+// let name = "Jonas";
+// admin = name;
+// alert(admin)
+
+// Teisingas uzvadinimas
+
+// let ourPlanetName = "Earth";
+// let currentUserName = "August";
+
+// Uppercase const?
+
