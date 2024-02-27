a06a69ce0d2df419e6d15111ddcc904cebdb9506
“Augustina <baugustina@gmail.com>
2024-02-27 19:55:03 +0200

    tue 27

---
+++ Javascript/Week 7/Paskaita 2/script.js
@@ -0,0 +1,66 @@
+let skaicius = 10;
+
+skaicius += 5;
+
+skaicius -=10;
+
+skaicius *=10;
+
+skaicius /= 5;
+
+skaicius %= 3;
+
+skaicius **=8;
+
+console.log(skaicius);
+
+const skaicius1 = 15;
+const skaicius2 = 9;
+
+const sum = skaicius1 + skaicius2;
+
+console.log(sum);
+
+let skaicius3 = 10;
+
+skaicius3++;
+
+console.log(skaicius3);
+
+console.log(5 < 8);
+console.log(8 == "8", "8 == 8");
+console.log(7 != 8);
+console.log(8 === "8", "8 == 8");
+
+// const userInput = prompt("Iveskite skaiciu");
+
+// typeof grazina kinatmojo tipa
+// +userInput pakeicia ivesta teksta i skaiciu
+// NaN - not a number type - number
+
+// console.log(+userInput);
+
+// let balance = 300;
+
+// const moneyToTransfer = prompt("Iveskite suma");
+
+// balance -= +moneyToTransfer;
+
+// console.log(balance);
+
+// let balance = 500;
+
+// const interestRate = prompt("Iveskite palukanu norma");
+
+// balance += balance * (interestRate / 100);
+
+// console.log(balance);
+
+// const word = "bananas";
+
+// const guess = prompt("Bandykite atspeti zodi");
+
+// console.log("spejimo rezultatas:", word === guess);
+
+// Uzduotys
+// Amzius
