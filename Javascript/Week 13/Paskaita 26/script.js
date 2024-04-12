28790ed244eaadfbde6292c8bfaeee14c49cc0e9
Augustina Baltrusaityte <baugustina@gmail.com>
2024-04-12 17:45:34 +0300

    check

--- Javascript/Week 13/Paskaita 26/script.js
+++ Javascript/Week 13/Paskaita 26/script.js
@@ -2,5 +2,6 @@ document.cookie = "name=august, expires=" + new Date(2024, 5, 10).toUTCString();
 
 console.log(new Date(2024, 4, 11).toUTCString());
 
+console.log("do i need a new laptop omg");
 
-
+console.log("checking if this works");
