f1db2025347bb363e3e6d79dcb7f46f8dfd71cef
Augustina Baltrusaityte <baugustina@gmail.com>
2024-04-26 00:20:23 +0300

    paskaita 1 practice

---
+++ Nodejs/Paskaita1practice/index.js
@@ -0,0 +1,10 @@
+import casual from "casual";
+
+const city = casual.city;
+
+console.log(city);
+console.log(Math.random());
+
+const parameters = `${casual.name_suffix} ${casual.first_name} ${casual.last_name}`;
+
+console.log(parameters);
