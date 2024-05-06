89cb9cfd7a19211808c8d3c348e3d9306effc908
Augustina Baltrusaityte <baugustina@gmail.com>
2024-05-06 20:01:29 +0300

    lesson6

---
+++ Nodejs/Paskaita5/server.js
@@ -0,0 +1,16 @@
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const PORT = process.env.port;
+
+const app = express();
+
+var a = hello;
+
+var b = hello;
+sferfer
+
+
+app.listen(PORT, () => console.log(`app listening on PORT ${PORT}`));
