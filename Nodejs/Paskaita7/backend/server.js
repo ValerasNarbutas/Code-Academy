1555cc7075041b1853e708097395ea4bc5e90755
Augustina Baltrusaityte <baugustina@gmail.com>
2024-05-09 19:41:03 +0300

    Thursday lesson

--- Nodejs/Paskaita7/backend/server.js
+++ Nodejs/Paskaita7/backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
+import cors from "cors";
 import router from "./routes/index.js";
 
 dotenv.config();
@@ -11,6 +12,7 @@ mongoose.connect(MONGO_URI, { dbName: "techShop" }).then(() => console.log("Conn
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(router); 
 
